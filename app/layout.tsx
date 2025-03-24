9a01c6ae14fd310c68d1696ad0bada193d828b15
YaIsrak <israkdreamstar@gmail.com>
2025-03-24 10:36:58 +0600

    fix 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,51 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import localFont from 'next/font/local';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
 
 const inter = Inter({
-	subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 const monument = localFont({
-	src: [
-		{
-			path: '../public/fonts/MonumentExtended-Regular.otf',
-			weight: '400',
-			style: 'normal',
-		},
-		{
-			path: '../public/fonts/MonumentExtended-Ultrabold.otf',
-			weight: '700',
-			style: 'normal',
-		},
-		{
-			path: '../public/fonts/MonumentExtended-black.otf',
-			weight: '900',
-			style: 'normal',
-		},
-	],
-	variable: '--font-bold-sans',
+  src: [
+    {
+      path: "../public/fonts/MonumentExtended-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/MonumentExtended-Ultrabold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/MonumentExtended-black.otf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-bold-sans",
 });
 
 export const metadata: Metadata = {
-	title: 'Altux Studio',
-	description:
-		'Crafting modern web experiences with cutting-edge development, UI/UX design, and innovative coding solutions.',
+  title: "Altux Studio",
+  description:
+    "Crafting modern web experiences with cutting-edge development, UI/UX design, and innovative coding solutions.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html
-			lang='en'
-			className='dark'
-			suppressHydrationWarning>
-			<body
-				className={`${inter.className} ${monument.variable} antialiased w-full h-full`}>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body
+        className={`${inter.className} ${monument.variable} h-full w-full antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
